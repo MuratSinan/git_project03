83cc32a76931127d367ba85291c2418678fb9d26
Murat Sinan <muratsinan099@gmail.com>
2020-03-17 00:53:44 +0100

    first branch

--- src/gitDay4Package/GitDay4.java
+++ src/gitDay4Package/GitDay4.java
@@ -2,4 +2,14 @@ package gitDay4Package;
 
 public class GitDay4 {
 
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println("local first commit master branch");
+		System.out.println("first branch");
+		
+	}
+	
+	
 }
